b5d7123c84641c7b351f924f2f772bd841a56cb0
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-02-03 17:56:28 +0700

    Todo only

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # fancy-to-do
 Fancy To-Do List API
+
+# Todo
+## Create Todo
+Membuat Todo
+
+URL : `/todos`
+
+Method : `POST`
+
+Request Body :
+```json
+{
+  "title": "<String>",
+  "description": "<String>",
+  "status": "<String>",
+  "due_date": "<Date>"
+}
+```
+
+Success Status Code : `201`
+
+Success Response :
+```json
+{
+  "id": "<INTEGER> <PRIMARY_KEY>",
+  "title": "<String>",
+  "description": "<String>",
+  "status": "<String>",
+  "due_date": "<Date>",
+  "createdAt": "<Date>",
+  "updatedAt": "<Date>"
+}
+```
+
+## Show All Todo
+Menampilkan semua Todo
+
+URL :
+
+Method :
+
+Request Body : 
+
+Success Status Code : 
+
+Success Response :
+
+## Show Todo by Id
+Menampilkan 1 Todo dengan Id tertentu
+
+URL : 
+
+Method : 
+
+Request Body : 
+
+Success Status Code : 
+
+Success Response : 
--- server/app.js
+++ server/app.js
@@ -1,20 +1,47 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+console.log(`"${process.env.NODE_ENV}"`);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// logErrors
+app.use(function (err, req, res, next) {
+  console.error(err)
+  console.error(err.status);
+  next(err)
+});
+// // clientErrorHandler
+// app.use(function (err, req, res, next) {
+//   if (req.xhr) {
+//     res.status(500).send({ error: 'Something failed!' })
+//   } else {
+//     next(err)
+//   }
+// });
+// errorHandler
+// app.use(function (err, req, res, next) {
+//   res.status(500)
+//   // res.render('error', { error: err })
+//   res.json({error: err});
+// });
+app.use(errorHandler);
 
 module.exports = app;
