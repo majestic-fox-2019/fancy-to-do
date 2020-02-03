60c4b9d63357b9143409f37528bdb87a5ca54168
thedunnaf <dunnaf.dev@gmail.com>
2020-02-03 16:41:15 +0700

    Solved

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,14 +1,33 @@
-const {Todo} = require("../models");
+const { Todo } = require("../models");
 
 class Controller {
+	static findOne(req, res, next) {
+		const id = req.params.id;
+		const where = {
+			where: {
+				id: id
+			}
+		};
+		Todo.findOne(where)
+			.then(data => {
+				if (!data) {
+					next({ name: "NotFound" });
+				} else {
+					res.status(200).json(data)
+				}
+			})
+			.catch(err => {
+				next(err);
+			});
+	}
 	static findAll(req, res, next) {
 		Todo.findAll()
-		.then(data => {
-			res.status(200).json(data);
-		})
-		.catch(err => {
-			next(err);
-		});
+			.then(data => {
+				res.status(200).json(data);
+			})
+			.catch(err => {
+				next(err);
+			});
 	}
 	static create(req, res, next) {
 		const obj = {
@@ -18,12 +37,12 @@ class Controller {
 			due_date: req.body.due_date
 		};
 		Todo.create(obj)
-		.then(data => {
-			res.status(201).json(data);
-		})
-		.catch(err => {
-			next(err);
-		});
+			.then(data => {
+				res.status(201).json(data);
+			})
+			.catch(err => {
+				next(err);
+			});
 	}
 	static update(req, res, next) {
 		const id = req.params.id;
@@ -36,20 +55,20 @@ class Controller {
 		const where = {
 			where: {
 				id: id
-			}, 
+			},
 			returning: true
 		};
 		Todo.update(obj, where)
-		.then(data => {
-			if(data[0] == 0) {
-				next({name:"NotFound"});
-			} else {
-				res.status(200).json(data[1]);
-			}
-		})
-		.catch(err => {
-			next(err);
-		});
+			.then(data => {
+				if (data[0] == 0) {
+					next({ name: "NotFound" });
+				} else {
+					res.status(200).json(data[1]);
+				}
+			})
+			.catch(err => {
+				next(err);
+			});
 	}
 	static destroy(req, res, next) {
 		const id = req.params.id;
@@ -59,17 +78,17 @@ class Controller {
 			}
 		};
 		Promise.all([Todo.findByPk(id), Todo.destroy(where)])
-		.then(data => {
-			if(data[1] == 0) {
-				next({name:"NotFound"})
-			} else {
-				res.status(200).json(data[0]);
-			}
-		})
-		.catch(err => {
-			next(err);
-		});
-	}	
+			.then(data => {
+				if (data[1] == 0) {
+					next({ name: "NotFound" })
+				} else {
+					res.status(200).json(data[0]);
+				}
+			})
+			.catch(err => {
+				next(err);
+			});
+	}
 }
 
 module.exports = Controller;
--- routes/todo.js
+++ routes/todo.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Controller = require("../controllers/todo");
 
 router.get("/", Controller.findAll);
+router.get("/:id", Controller.findOne)
 router.post("/", Controller.create);
 router.put("/:id", Controller.update);
 router.delete("/:id", Controller.destroy);
