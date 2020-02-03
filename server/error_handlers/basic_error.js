c60f664ddb11ff1e5ac663f78a8f72b9ae2fa08b
Onesinus SPT <onesinus231@gmail.com>
2020-02-03 18:22:48 +0700

    Documentations

--- server/error_handlers/basic_error.js
+++ server/error_handlers/basic_error.js
@@ -2,17 +2,18 @@ const express   = require('express');
 const app       = express();
 
 function basicError(err, req, res, next){
+    console.log(err.message);
     try {
         if (err.statusCode) { // Handling Error with status code and message, such as 404
             res.status(err.statusCode).json({Error: err.message});
-        }else if(err.message) { // Handling validations error from model
+        }else if(err.message) { // Handling validations error from model, 400
             let arrValidations = [];
             err.errors.forEach(validation => {
                 arrValidations.push(validation.message);
             });
             res.status(400).json({Validations: arrValidations});                        
         }            
-    } catch (error) {
+    } catch (error) { // 500
         res.status(500).json({msg:"Something went wrong... it's not your fault, that's our server problem!"});
     }
 }
