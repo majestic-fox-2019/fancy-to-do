9bb4ae203ba4e94f2d18f26a43691f2f352f570f
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-02-03 17:54:02 +0700

    middlewares

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+function errorHandler(err, req, res, next) {
+  require("sequelize").error
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(res.locals);
+}
+
+module.exports = errorHandler;
