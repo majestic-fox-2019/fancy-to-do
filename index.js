fa9c73a97b8686721d5479e736b4e017f777ebd9
Adam Rafiandri <rafiandria23@gmail.com>
2020-02-04 15:42:38 +0700

    Removed error handler

--- index.js
+++ index.js
@@ -3,33 +3,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const { todoRouter } = require("./routes/todo-router");
+const { indexRouter } = require("./routes/index-router");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/todos", todoRouter);
-
-app.use((err, req, res, next) => {
-  if (err) {
-    // res.status(err.statusCode).json(err);
-    switch (err.statusCode) {
-      case 404:
-        err.message = "Error! Not Found.";
-        res.status(err.statusCode).json(err);
-        break;
-      
-      case 500:
-        err.message = "Internal Server Error!";
-        res.status(err.statusCode).json(err);
-      break;
-    
-      default:
-        res.status(err.statusCode).json(err);
-        break;
-    }
-  }
-});
+app.use("/", indexRouter);
 
 
 app.listen(port, () => {
