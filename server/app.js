88f4f063355f11b2305672e2eee740a82de65078
Onesinus SPT <onesinus231@gmail.com>
2020-02-05 16:51:23 +0700

    Backend nambahin cors

--- server/app.js
+++ server/app.js
@@ -13,13 +13,18 @@ const UserController    = require('./controllers/UserController');
 
 const app               = express();
 const dotenv            = require('dotenv');
+const cors              = require('cors');
+
+
 
 dotenv.config();
 
 const port      = process.env.PORT;
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+
 
 app.post('/users/register', UserController.register);
 app.post('/users/login', UserController.login);
