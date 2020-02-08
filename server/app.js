764c99ad28bb5e1ff8c01eef6be2a42bb0016737
Okka Linardi <okka.linardi@gmail.com>
2020-02-08 15:59:52 +0700

    Changed port from static to dynamic

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const router = require('./router')
-const port = 3000
+const port = process.env.PORT || 3000
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
 
