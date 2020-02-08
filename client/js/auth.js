47f6ba4b1105384c1f3c8854b229270a65d8de11
Mazda94 <m.alli9401@gmail.com>
2020-02-08 20:54:34 +0700

    initial commit

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,14 +1,17 @@
 function onSignIn(googleUser) {
     const id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token)
     $.ajax({
         type: "post",
         url: `${endpoint}/google-signin`,
         data: { id_token }
     }).done(response => {
+        console.log(response)
         localStorage.setItem('token', response.token)
         localStorage.setItem('email', response.email)
         userloggedIn()
     }).fail(err => {
+        console.log(err)
         Request.createError(err)
     })
 }
