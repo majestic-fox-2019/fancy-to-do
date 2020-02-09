c3bdf495c85eca61dd4ead13c840d0e28e4459d4
fadhilahrayafi <fadhilahrayafi@gmail.com>
2020-02-09 20:02:40 +0700

    heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
 }
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const routes = require('./routes/index')
 const errorHandler = require('./middlewares/errorhandler')
 const cors = require('cors')
