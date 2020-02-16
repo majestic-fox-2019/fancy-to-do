314d317a19cadcb1c2ce9d25c0124a2a92b3610d
muhafaa <muhafa99@gmail.com>
2020-02-16 21:43:59 +0700

    heroku deploy

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
-const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 app.use(cors())
